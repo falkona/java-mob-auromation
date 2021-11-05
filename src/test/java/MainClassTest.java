7160799722691691ed1b77df1cf03abda5cce878
Daria Nos <ddnos@alfabank.ru>
2021-11-05 20:22:51 +0300

    tests 2,3

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,13 +1,30 @@
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
 
+    private MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
         assertEquals(14, mainClass.getLocalNumber(), "Фактический результат не равен 14");
     }
 
+    @Test
+    public void testGetClassNumber() {
+        assertTrue(mainClass.getClassNumber() > 45,"Фактический результат меньше 45");
+    }
+
+    @Test
+    public void testGetClassString() {
+        String classString = mainClass.getClassString();
+        assertThat("Подстрока не найдена", classString, anyOf(
+                containsString("hello"),
+                containsString("Hello")));
+    }
 }
